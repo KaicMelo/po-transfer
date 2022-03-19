2bc176fbf5a979299dfd534ec6811dbfb569595c
kaic melo <kaic.melo@totvs.com.br>
2022-03-18 23:46:21 -0300

    aula 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ import { PoMenuItem } from '@po-ui/ng-components';
 export class AppComponent {
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) }
+    { label: 'Home', action: () => alert("Hello World") },
+    { label: 'Menu', action: () => alert("Hello World") }
   ];
 
   private onClick() {
