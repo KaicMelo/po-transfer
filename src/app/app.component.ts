528d1dab49da4dfff6fb2c44fef3140a19128be5
Kaic Melo Santos <kakakaic@yahoo.com.br>
2022-04-06 23:12:03 -0300

    Creating template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { PoMenuItem } from '@po-ui/ng-components';
+import { PoDynamicFormField, PoMenuItem } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,11 @@ export class AppComponent {
     { label: 'Inicio', action: () => alert('Hello world') }
   ];
 
-  private onClick() {
-    alert('Clicked in menu item')
-  }
+  propertyForm: Array<PoDynamicFormField> = [
+    { property: 'sender', label: 'Remetente', placeholder: 'Remetente', required: true },
+    { property: 'recipient', label: 'Destinatário', placeholder: 'Destinatário', required: true },
+    { property: 'money', label: 'Valor', type: 'currency', placeholder: 'Valor', required: true },
+    { property: 'description', label: 'Descrição', required: true },
+  ]
 
 }
