409774418893774b2d042ae0453b60eac50f7787
Kaic Melo Santos <kakakaic@yahoo.com.br>
2022-04-16 12:48:20 -0300

    including po button and gridColunm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,47 @@ import { PoDynamicFormField, PoMenuItem } from '@po-ui/ng-components';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Inicio', action: () => alert('Hello world') }
+    { label: 'Inicio', action: () => alert('Hello world') },
   ];
 
   propertyForm: Array<PoDynamicFormField> = [
-    { property: 'sender', label: 'Remetente', placeholder: 'Remetente', required: true },
-    { property: 'recipient', label: 'Destinatário', placeholder: 'Destinatário', required: true },
-    { property: 'money', label: 'Valor', type: 'currency', placeholder: 'Valor', required: true },
-    { property: 'description', label: 'Descrição', required: true },
-  ]
+    {
+      property: 'sender',
+      label: 'Remetente',
+      placeholder: 'Remetente',
+      required: true,
+      gridColumns: 4,
+    },
+    {
+      property: 'recipient',
+      label: 'Destinatário',
+      placeholder: 'Destinatário',
+      required: true,
+      gridColumns: 4,
+    },
+    {
+      property: 'money',
+      label: 'Valor',
+      type: 'currency',
+      placeholder: 'Valor',
+      required: true,
+      gridColumns: 4,
+    },
+    {
+      property: 'description',
+      label: 'Descrição',
+      required: true,
+      gridColumns: 12,
+      rows:5,
+      placeholder:'Descrição'
+    },
+  ];
 
+  save(){
+    alert('ok')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PoModule, PoDynamicModule } from '@po-ui/ng-components';
+import { PoModule, PoDynamicModule, PoButtonModule } from '@po-ui/ng-components';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -13,7 +13,8 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     PoModule,
     RouterModule.forRoot([]),
-    PoDynamicModule
+    PoDynamicModule,
+    PoButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
